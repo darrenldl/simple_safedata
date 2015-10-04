2a85eda39bce282439b5ef1fa34a0d0baef0b3e3
darrenldl <dldldev@yahoo.com>
2015-10-04 22:47:36 +1100

    removed use of nested functions

--- simple_safedata.h
+++ simple_safedata.h
@@ -38,7 +38,7 @@
 
 //#define SIMPLE_SAFEDATA_SILENT
 
-#define SIMPLE_SAFEDATA_DISABLE
+//#define SIMPLE_SAFEDATA_DISABLE
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -49,6 +49,8 @@
 #include <float.h>
 
 #include <string.h>
+
+#include <stdarg.h>
 
 #include "simple_bitmap.h"
 
@@ -66,6 +68,7 @@
    #define sfd_var_get_up_bnd(...)  0
    #define sfd_var_set_lo_bnd(...)  0
    #define sfd_var_set_up_bnd(...)  0
+   #define sfd_var_def_con(...)
    #define sfd_var_add_con(...)     0
    #define sfd_var_enforce_con(...) 1
    #define sfd_arr_dec_sta(type, name, size)                      type name[size]
@@ -74,6 +77,8 @@
    #define sfd_arr_read(name, indx)                               name[indx]
    #define sfd_arr_write(name, indx, in_val)                      (name[indx] = in_val)
    #define sfd_arr_wipe(...)        0
+   #define sfd_arr_def_con_ele(...)
+   #define sfd_arr_def_con_arr(...)
    #define sfd_arr_enforce_con(...) 1
    #define sfd_arr_add_con_ele(...) 0
    #define sfd_arr_add_con_arr(...) 0
@@ -105,7 +110,7 @@
       type up_bnd;         \
       type lo_bnd;         \
       int (*constraint) (type);\
-      char* con_expr;      \
+      char* con_in_effect; \
    } name;\
    name.flags = SFD_FL_READ | SFD_FL_WRITE | SFD_FL_CON;\
    name.lo_bnd =\
@@ -144,7 +149,7 @@
          long double    : LDBL_MAX  \
       )\
    ;\
-   name.constraint = &sfd_dummy;
+   name.constraint = &sfd_dummy_con_var;
 
 #define sfd_var_read(name) \
    (name.flags & SFD_FL_READ? \
@@ -197,19 +202,21 @@
 #define sfd_var_set_up_bnd(name, in_val) \
    (name.up_bnd = in_val)
 
-#define sfd_var_add_con(name, type, arg_name, expr) \
-   int constraint##_sfd_var_##name (type arg_name) {\
+#define sfd_var_def_con(con_name, type, arg_name, expr)\
+   int sfd_con_##con_name##_var (type arg_name) {\
       return (expr);\
-   }\
-   name.constraint = &constraint##_sfd_var_##name;\
-   name.con_expr = #expr;
+   }
+
+#define sfd_var_add_con(name, con_name) \
+   name.constraint = &sfd_con_##con_name##_var;\
+   name.con_in_effect = #con_name;
 
 #define sfd_var_enforce_con(name) \
    (name.constraint(name.val)? \
       0\
    :\
        0* sfd_printf("Constraint failed : file : %s, line : %d\n", __FILE__, __LINE__)\
-      +0* sfd_printf("Constraint in effect : %s\n", name.con_expr)\
+      +0* sfd_printf("Constraint in effect : %s\n", name.con_in_effect)\
       +0* sfd_force_exit()\
    )
 
@@ -220,16 +227,16 @@
       uint_fast32_t size;  \
       simple_bitmap init_map;\
       map_block temp;      \
-      int (*constraint) ();\
-      char* con_expr;      \
+      int (*constraint) (int, ...);\
+      char* con_in_effect;      \
    } name;\
    map_block name##_sfd_raw_init_map [get_bitmap_map_block_number(in_size)];\
    type name##_sfd_arr [in_size];\
-   name.flags = SFD_FL_READ | SFD_FL_WRITE;\
+   name.flags = SFD_FL_READ | SFD_FL_WRITE | SFD_FL_CON;\
    name.start = name##_sfd_arr;\
    name.size = in_size;\
    bitmap_init(&name.init_map, name##_sfd_raw_init_map, NULL, in_size, 0);\
-   name.constraint = &sfd_dummy;
+   name.constraint = &sfd_dummy_con_arr;
 
 #define sfd_arr_dec_dyn(type, name, in_size)\
    struct {\
@@ -238,8 +245,8 @@
       uint_fast32_t size;  \
       simple_bitmap init_map;\
       map_block temp;      \
-      int (*constraint) ();\
-      char* con_expr;      \
+      int (*constraint) (int, ...);\
+      char* con_in_effect;      \
    } name;\
    map_block* name##_sfd_raw_init_map = (map_block*) malloc(sizeof(map_block) * get_bitmap_map_block_number(in_size));\
    name.start = (type*) malloc(sizeof(type) * in_size);\
@@ -247,13 +254,13 @@
       sfd_printf("sfd_arr_dec_dyn : malloc failed : file : %s, line : %d\n", __FILE__, __LINE__);\
       name.flags = 0;\
       name.size = 0;\
-      name.constraint = &sfd_dummy;\
+      name.constraint = &sfd_dummy_con_arr;\
    }\
    else {\
-      name.flags = SFD_FL_READ | SFD_FL_WRITE;\
+      name.flags = SFD_FL_READ | SFD_FL_WRITE | SFD_FL_CON;\
       name.size = in_size;\
       bitmap_init(&name.init_map, name##_sfd_raw_init_map, NULL, in_size, 0);\
-      name.constraint = &sfd_dummy;\
+      name.constraint = &sfd_dummy_con_arr;\
    }
 
 #define sfd_arr_dec_man(type, name, in_size, bmp_start, arr_start)\
@@ -263,14 +270,14 @@
       uint_fast32_t size;  \
       simple_bitmap init_map;\
       map_block temp;      \
-      int (*constraint) ();\
-      char* con_expr;      \
+      int (*constraint) (int, ...);\
+      char* con_in_effect;      \
    } name;\
-   name.flags = SFD_FL_READ | SFD_FL_WRITE;\
+   name.flags = SFD_FL_READ | SFD_FL_WRITE | SFD_FL_CON;\
    name.start = arr_start;\
    name.size = in_size;\
    bitmap_init(&name.init_map, bmp_start, NULL, in_size, 0);\
-   name.constraint = &sfd_dummy;
+   name.constraint = &sfd_dummy_con_arr;
 
 #define sfd_arr_read(name, indx) \
    (name.flags & SFD_FL_READ? \
@@ -300,7 +307,7 @@
       (indx < name.size? \
              (name.start[indx] = in_val)\
          +0* bitmap_write(&name.init_map, indx, 1, 0)\
-         +0* sfd_arr_enforce_con(name)\
+         +0* (name.flags & SFD_FL_CON? sfd_arr_enforce_con(name) : 0)\
       :\
           0* sfd_printf("Index out of bound : file : %s, line : %d\n", __FILE__, __LINE__)\
          +0* sfd_force_exit()\
@@ -320,36 +327,48 @@
    )
 
 #define sfd_arr_enforce_con(name) \
-   (name.constraint()? \
+   (name.constraint(0, name.start, name.size)? \
       0\
    :\
        0* sfd_printf("Constraint failed : file : %s, line : %d\n", __FILE__, __LINE__)\
-      +0* sfd_printf("Constraint in effect : %s\n", name.con_expr)\
+      +0* sfd_printf("Constraint in effect : %s\n", name.con_in_effect)\
       +0* sfd_force_exit()\
    )
 
-#define sfd_arr_add_con_ele(name, type, arg_name, expr) \
-   int constraint##_sfd_arr_##name (type arg_name) {\
+#define sfd_arr_def_con_ele(con_name, type, arg_name, expr) \
+   int sfd_con_##con_name##_per_element (type arg_name) {\
       return (expr);\
    }\
-   int con_loop##_sfd_arr_##name () {\
+   int sfd_con_##con_name##_arr_loop (int N, ...) {\
+      va_list args;\
+      va_start(args, N);\
       int i = 0;\
-      for (i = 0; i < name.size; i++) {\
-         if ( ! constraint##_sfd_arr_##name(name.start[i])) {\
+      type* start = va_arg(args, type*);\
+      int size = va_arg(args, uint_fast32_t);\
+      for (i = 0; i < size; i++) {\
+         if ( ! sfd_con_##con_name##_per_element(start[i])) {\
             return 0;\
          }\
       }\
       return 1;\
-   }\
-   name.constraint = &con_loop##_sfd_arr_##name;\
-   name.con_expr = #expr;
-   
-#define sfd_arr_add_con_arr(name, func) \
-   int constraint##_sfd_var_##name () {\
-      return func(name.start, name.size);\
-   }\
-   name.constraint = &constraint##_sfd_var_##name;\
-   name.con_expr = #func;
+   }
+
+#define sfd_arr_def_con_arr(con_name, type, func_name) \
+   int sfd_con_##con_name##_array_wise (int N, ...) {\
+      va_list args;\
+      va_start(args, N);\
+      type* start = va_arg(args, type*);\
+      int size = va_arg(args, uint_fast32_t);\
+      return func(start, size);\
+   }
+
+#define sfd_arr_add_con_ele(name, con_name) \
+   name.constraint = &sfd_con_##con_name##_arr_loop;\
+   name.con_in_effect = #con_name;
+
+#define sfd_arr_add_con_arr(name, con_name) \
+   name.constraint = &sfd_con_##con_name##_array_wise;\
+   name.con_in_effect = #con_name;
 
 #define sfd_arr_get_size(name) \
    (name.size)
@@ -359,7 +378,11 @@ static int sfd_force_exit() {
    return 0;
 }
 
-static int sfd_dummy() {
+static int sfd_dummy_con_var(int dum) {
+   return 1;
+}
+
+static int sfd_dummy_con_arr(int dum, ...) {
    return 1;
 }
 
